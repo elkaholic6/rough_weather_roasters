53a425379eb9a905766e780a0fc445f59ccd9730
elkaholic6 <wyattchamberlin6@gmail.com>
2025-12-12 07:06:15 -0500

    Set up GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/rough_weather_roasters/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
